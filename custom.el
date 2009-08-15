dcc4e99d311057f33ab56bf9333eced9a7a8b881
Daemian Mack <daemianmack@gmail.com>
2009-08-15 09:25:19 -0400

    Change just about anything that's dark blue be bright blue instead. Whoa, readable minibuffer prompt all the suddenly.

--- custom.el
+++ custom.el
@@ -10,4 +10,12 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(comint-highlight-prompt ((t (:foreground "brightblue"))))
+ '(custom-comment-tag ((((class color) (background light)) (:foreground "brightblue"))))
+ '(custom-group-tag ((((min-colors 88) (class color) (background light)) (:foreground "brightblue" :weight bold :height 1.2))))
+ '(custom-variable-tag ((((min-colors 88) (class color) (background light)) (:inherit variable-pitch :foreground "brightblue" :weight bold :height 1.2))))
+ '(font-lock-function-name-face ((((class color) (min-colors 88) (background light)) (:foreground "brightblue"))))
+ '(hi-blue-b ((((min-colors 88)) (:foreground "brightblue" :weight bold))))
+ '(link ((((class color) (min-colors 88) (background light)) (:foreground "brightblue" :underline t))))
+ '(minibuffer-prompt ((t (:foreground "brightblue"))))
  '(region ((((class color) (min-colors 88) (background light)) (:background "brightblack")))))
