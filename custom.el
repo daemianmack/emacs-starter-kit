dc236f93fa5d53c0ac97febb66ef45df358e622d
Daemian Mack <daemianmack@gmail.com>
2009-08-17 18:17:46 -0400

    Make split-window borders brightblue, make buffer titles a bit nicer, turn off underlining so nav isn't all ugly.

--- custom.el
+++ custom.el
@@ -10,12 +10,22 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(button ((((supports :underline t)) nil)))
  '(comint-highlight-prompt ((t (:foreground "brightblue"))))
  '(custom-comment-tag ((((class color) (background light)) (:foreground "brightblue"))))
  '(custom-group-tag ((((min-colors 88) (class color) (background light)) (:foreground "brightblue" :weight bold :height 1.2))))
  '(custom-variable-tag ((((min-colors 88) (class color) (background light)) (:inherit variable-pitch :foreground "brightblue" :weight bold :height 1.2))))
  '(font-lock-function-name-face ((((class color) (min-colors 88) (background light)) (:foreground "brightblue"))))
+ '(header-line ((default (:background "brightblue")) (((type tty)) nil)))
  '(hi-blue-b ((((min-colors 88)) (:foreground "brightblue" :weight bold))))
+ '(highlight ((((class color) (min-colors 88) (background light)) (:background "darkseagreen2" :foreground "black"))))
+ '(italic ((((supports :underline t)) (:underline nil))))
  '(link ((((class color) (min-colors 88) (background light)) (:foreground "brightblue" :underline t))))
  '(minibuffer-prompt ((t (:foreground "brightblue"))))
- '(region ((((class color) (min-colors 88) (background light)) (:background "brightblack")))))
+ '(mode-line ((((class color) (min-colors 88)) (:background "blue" :foreground "white" :box (:line-width -1 :style released-button)))))
+ '(mode-line-highlight ((((class color) (min-colors 88)) (:foreground "brightgreen" :box (:line-width 1 :color "grey40" :style released-button)))))
+ '(mode-line-inactive ((default (:foreground "brightblack")) (nil nil)))
+ '(region ((((class color) (min-colors 88) (background light)) (:background "brightblack"))))
+ '(secondary-selection ((((class color) (min-colors 88) (background light)) (:background "yellow1" :foreground "black"))))
+ '(underline ((((supports :underline t)) (:underline nil))))
+ '(vertical-border ((((type tty)) (:background "black" :foreground "brightblack")))))
