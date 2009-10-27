7761d0f1292f8d44367605b69f2918256280e624
Daemian Mack <dmack@navisite.com>
2009-10-27 13:51:13 -0400

    More recentf.

--- init.el
+++ init.el
@@ -123,6 +123,7 @@
 
 ;; Controls how many non-permanent entries are shown in the recent-files list. The default is 15. 
 (setq recent-files-number-of-entries 100)
+(setq recentf-max-menu-items 100)
 
 
 ;; Start server.
