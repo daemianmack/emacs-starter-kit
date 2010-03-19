1972c859d192a6b35d51c5e8f8b10d134534a694
Daemian Mack <dmack@navisite.com>
2010-03-19 09:40:33 -0400

    Use emacs-desktop to preserve buffers/search history/etc across app reloads.

--- init.el
+++ init.el
@@ -329,5 +329,29 @@
 ;; Built-in keybinding for dot-mode-execute (C-.) isn't detected for me.
 (global-set-key (kbd "C-M-r") 'dot-mode-execute)
 
+
+;; save a list of open files in ~/.emacs.desktop
+;; save the desktop file automatically if it already exists
+;; M-x desktop-save necessary to first create this file, auto-updates thereafter.
+(setq desktop-save 'if-exists)
+(desktop-save-mode 1)
+
+;; save a bunch of variables to the desktop file
+;; for lists specify the len of the maximal saved data also
+(setq desktop-globals-to-save
+      (append '((extended-command-history . 30)
+                (file-name-history        . 100)
+                (grep-history             . 30)
+                (compile-history          . 30)
+                (minibuffer-history       . 50)
+                (query-replace-history    . 60)
+                (read-expression-history  . 60)
+                (regexp-history           . 60)
+                (regexp-search-ring       . 20)
+                (search-ring              . 20)
+                (shell-command-history    . 50)
+                tags-file-name
+                register-alist)))
+
 ;;; init.el ends here
 (put 'downcase-region 'disabled nil)
