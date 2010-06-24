ba4ed794c841636cee28050723ce1cc06768a796
Daemian Mack <dmack@navisite.com>
2010-06-23 20:58:09 -0400

    Turn on column-number-mode.

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
  '(gud-gdb-command-name "gdb --annotate=1")
  '(ibuffer-formats (quote ((mark modified read-only git-status-mini " " (name 18 18 :left :elide) " " (size 9 -1 :right) " " (mode 16 16 :left :elide) " " (git-status 8 8 :left) " " filename-and-process))))
  '(large-file-warning-threshold nil)
