27113d8cdb1dcb07ec872bc9485cca253f136ea8
Daemian Mack <dmack@navisite.com>
2010-06-30 14:49:08 -0400

    Add pyflakes and pylint support.

--- init.el
+++ init.el
@@ -417,6 +417,30 @@
   (mysql)
   (django-shell))
 
+(when (load "flymake" t)
+  (defun flymake-pylint-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list "epylint" (list local-file))))
+
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pylint-init)))
+
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+;; Make sure it's not a remote buffer or flymake would not work
+    (when (not (subsetp (list (current-buffer)) (tramp-list-remote-buffers)))
+      (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                         'flymake-create-temp-inplace))
+             (local-file (file-relative-name
+                          temp-file
+                          (file-name-directory buffer-file-name))))
+        (list "pyflakes-2.6" (list local-file)))))
+  (add-to-list 'flymake-allowed-file-name-masks
+                              '("\\.py\\'" flymake-pyflakes-init))) 
 ;;; init.el ends here
 (put 'downcase-region 'disabled nil)
 
