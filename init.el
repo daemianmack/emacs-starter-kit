654a382f83743c6d5c8a76279d693253f64fee90
Daemian Mack <dmack@federatedmedia.net>
2012-05-18 12:46:31 -0400

    Clean up pyflakes call -- remove duplicate file and redundant epylint version of function.

--- init.el
+++ init.el
@@ -496,30 +496,18 @@
   (mysql)
   (django-shell))
 
-(load-library "flymake-cursor")
+(load-library "elpa-to-submit/flymake-cursor")
 (when (load "flymake" t)
-  (defun flymake-pylint-init ()
+  (defun flymake-pyflakes-init ()
+    ;; Make sure it's not a remote buffer or flymake would not work
     (let* ((temp-file (flymake-init-create-temp-buffer-copy
                        'flymake-create-temp-inplace))
            (local-file (file-relative-name
                         temp-file
                         (file-name-directory buffer-file-name))))
-      (list "epylint" (list local-file))))
-
+      (list "pyflakes" (list local-file))))
   (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pylint-init)))
-
-(when (load "flymake" t)
-  (defun flymake-pyflakes-init ()
-;; Make sure it's not a remote buffer or flymake would not work
-      (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                         'flymake-create-temp-inplace))
-             (local-file (file-relative-name
-                          temp-file
-                          (file-name-directory buffer-file-name))))
-        (list "pyflakes" (list local-file))))
-  (add-to-list 'flymake-allowed-file-name-masks
-                              '("\\.py\\'" flymake-pyflakes-init))) 
+               '("\\.py\\'" flymake-pyflakes-init))) 
 
 (defun my-isearch-word-at-point ()
   (interactive)
