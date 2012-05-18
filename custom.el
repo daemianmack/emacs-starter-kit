4801a666e74ce8e0d1e7460cb562952274fefccf
Daemian Mack <dmack@federatedmedia.net>
2012-05-18 11:16:24 -0400

    custom.el autogenerated comment block seems to have gotten an update.

--- custom.el
+++ custom.el
@@ -62,10 +62,10 @@
  '(which-function-mode t)
  '(windmove-wrap-around t))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
  '(button ((((supports :underline t)) nil)))
  '(comint-highlight-prompt ((t (:foreground "brightblue"))))
  '(custom-comment-tag ((((class color) (background light)) (:foreground "brightblue"))))
