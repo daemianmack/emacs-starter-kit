216cc3c3283d95317bd49c385c7f612624782c09
Daemian Mack <dmack@rpath.com>
2012-06-20 15:55:03 -0400

    Snip now-orphaned shorten-directory func.

--- init.el
+++ init.el
@@ -700,20 +700,6 @@
 )))
 
 
-;; Helper function
-(defun shorten-directory (dir max-length)
-  "Show up to `max-length' characters of a directory name `dir'."
-  (let ((path (reverse (split-string (abbreviate-file-name dir) "/")))
-        (output ""))
-    (when (and path (equal "" (car path)))
-      (setq path (cdr path)))
-    (while (and path (< (length output) (- max-length 4)))
-      (setq output (concat (car path) "/" output))
-      (setq path (cdr path)))
-    (when path
-      (setq output (concat ".../" output)))
-    output))
-
 ;; Extra mode line faces
 (make-face 'mode-line-read-only-face)
 (make-face 'mode-line-modified-face)
