506cbb9b3d0e10638bf8c4c1bda26fef24fbea6a
Relevance Pair <pair@thinkrelevance.com>
2013-02-04 12:45:22 -0500

    Tweak paren-matching and idle-highlight faces.

--- daemian.el
+++ daemian.el
@@ -141,11 +141,6 @@
 (show-paren-mode t)                 ; turn paren-mode on
 (setq show-paren-style 'expression) ; alternatives are 'parenthesis' and 'mixed'
 
-(set-face-background 'show-paren-match-face "#090909")
-(set-face-foreground 'show-paren-match-face "#3e00f8")
-(set-face-attribute 'show-paren-match-face nil
-        :weight 'normal :underline nil :overline nil :slant 'normal)
-
 (set-face-foreground 'show-paren-mismatch-face "white")
 (set-face-background 'show-paren-mismatch-face "red")
 (set-face-attribute 'show-paren-mismatch-face nil
