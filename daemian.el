eb14736c620a594c03d984185cefa174086ce93f
Daemian Mack and Yoko Harada <pair+dm+yh@thinkrelevance.com>
2013-02-11 05:53:15 -0500

    Move paren-matching into custom.el, tweak faces.

--- daemian.el
+++ daemian.el
@@ -136,16 +136,6 @@
 (setq auto-save-file-name-transforms
       `((".*" ,user-temporary-file-directory t)))
 
-;; paren matching
-(setq show-paren-delay 0)           ; how long to wait?
-(show-paren-mode t)                 ; turn paren-mode on
-(setq show-paren-style 'expression) ; alternatives are 'parenthesis' and 'mixed'
-
-(set-face-foreground 'show-paren-mismatch-face "white")
-(set-face-background 'show-paren-mismatch-face "red")
-(set-face-attribute 'show-paren-mismatch-face nil
-                    :weight 'bold :underline nil :overline nil :slant 'normal);;; init.el ends here
-
 (require 'nav)
 (require 'python-mode)
 
