a00c931c2e756692c098d8cfd29772f9fd795ff6
Daemian Mack <daemianmack@gmail.com>
2014-11-04 15:15:47 -0500

    Fix magit faces on pairhost. This shouldn't be necessary. (?)

--- custom.el
+++ custom.el
@@ -171,4 +171,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(diff-refine-added ((t nil)))
+ '(diff-refine-removed ((t nil)))
+ '(magit-diff-add ((t (:foreground "green"))))
+ '(magit-diff-del ((t (:foreground "red")))))
