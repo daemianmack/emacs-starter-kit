8b48d7a02bbf0dad94f08ddd72bd3e3b7f21dfa0
Daemian Mack <daemianmack@gmail.com>
2015-03-20 13:48:34 -0400

    Experimental ac-cider settings.

--- experimental.el
+++ experimental.el
@@ -17,3 +17,14 @@
       helm-file-cache-fuzzy-match t
       helm-imenu-fuzzy-match t
       helm-locate-fuzzy-match t)
+(require 'ac-cider)
+(add-hook 'cider-mode-hook 'ac-flyspell-workaround)
+(add-hook 'cider-mode-hook 'ac-cider-setup)
+(add-hook 'cider-repl-mode-hook 'ac-cider-setup)
+(eval-after-load "auto-complete"
+    '(add-to-list 'ac-modes 'cider-mode))
+(require 'auto-complete-config)
+(ac-config-default)
+(setq ac-auto-start nil)
+(setq ac-quick-help-delay 0.3)
+(setq ac-trigger-key "M-/")
