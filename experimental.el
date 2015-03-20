34a493984946744b91a5d950ec2defb50ad80b2f
Daemian Mack <daemianmack@gmail.com>
2015-03-20 13:40:28 -0400

    Nix experimental cruft.

--- experimental.el
+++ experimental.el
@@ -7,17 +7,5 @@
 
 (global-set-key (kbd "C-c f") 'recentf-ido-find-file)
 
-;(setq nrepl-hide-special-buffers t)
-;(setq cider-show-error-buffer nil)
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
-
-
-(global-aggressive-indent-mode)
-
-
-(require 'comment-dwim-2)
-(define-key cider-mode-map (kbd "M-;") 'comment-dwim-2)
-(setq comment-dwim-2--inline-comment-behavior 'reindent-comment)
-
-(global-set-key (kbd "C-c C-c") '(lambda () (interactive) (message "Here is a message.")))
 
