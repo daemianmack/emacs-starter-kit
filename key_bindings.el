6ef57c061b4403841daf5b536ee6fad435f6e48b
Daemian Mack <daemianmack@gmail.com>
2015-03-20 13:59:55 -0400

    Add whitespace-cleanup key.

--- key_bindings.el
+++ key_bindings.el
@@ -135,3 +135,5 @@
       (kill-region (region-beginning) (region-end))
     (backward-kill-word arg)))
 (global-set-key (kbd "C-w") 'backward-kill-word-or-kill-region)
+
+(global-set-key (kbd "C-c w") 'whitespace-cleanup)
