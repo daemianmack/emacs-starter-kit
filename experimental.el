80f3f35014c4971fcb2a8cb699dba6c72b495541
Daemian Mack <daemianmack@gmail.com>
2018-01-26 15:51:21 -0500

    Whitespace.

--- experimental.el
+++ experimental.el
@@ -423,7 +423,7 @@
   (defadvice magit-status (around magit-fullscreen activate)
     (window-configuration-to-register :magit-fullscreen)
     ad-do-it
-          (delete-other-windows))
+    (delete-other-windows))
   ;; restore previously hidden windows
   ;; TODO HACK
   (defadvice magit-quit-window (around magit-restore-screen activate)
