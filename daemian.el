d60632db10f351f2be9a70e729079ceea5d4aec8
Daemian Mack <daemianmack@gmail.com>
2018-07-25 12:36:53 -0400

    Nix flaky idea of eagerly loading all elpa/* libraries.

--- daemian.el
+++ daemian.el
@@ -1,12 +1,10 @@
 (setq dotfiles-dir (file-name-directory
                      (or (buffer-file-name) load-file-name)))
 
-(let ((default-directory (concat dotfiles-dir "elpa")))
-  (setq load-path
-        (append
-         (let ((load-path (copy-sequence load-path))) ;; Shadow
-           (normal-top-level-add-subdirs-to-load-path))
-         load-path)))
+(setq load-path
+      (append
+       (concat dotfiles-dir "elpa")
+       load-path))
 
 (setq custom-file (concat dotfiles-dir "custom.el"))
 (load custom-file 'noerror)
