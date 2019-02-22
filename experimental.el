43490d1a4ededc598a6595bf7930ece07598889d
Daemian Mack <daemianmack@gmail.com>
2019-02-22 08:11:21 -0500

    Google translate workaround for transient wierdness.

--- experimental.el
+++ experimental.el
@@ -923,7 +923,16 @@
   :config
   (require 'google-translate-smooth-ui)
   (validate-setq google-translate-translation-directions-alist '(("da" . "en")))
-  (validate-setq google-translate-output-destination 'echo-area))
+  (validate-setq google-translate-output-destination 'echo-area)
+  ;; Temporary workaround: https://github.com/atykhonov/google-translate/issues/52#issuecomment-423870290
+  (when (and (string-match "0.11.14"
+                           (google-translate-version))
+             (>= (time-to-seconds)
+                 (time-to-seconds
+                  (encode-time 0 0 0 23 9 2018))))
+    (defun google-translate--get-b-d1 ()
+      ;; TKK='427110.1469889687'
+      (list 427110 1469889687))))
 
 (use-package browse-kill-ring :ensure t
   :config
