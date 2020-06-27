412f352341bd0e690dac5146ab86666387578eee
Daemian Mack <daemianmack@gmail.com>
2020-06-27 11:17:34 -0400

    Nix mislocated 'custom' cruft

--- init.el
+++ init.el
@@ -60,23 +60,4 @@
 ;; Make gc pauses faster by decreasing the threshold.
 (setq gc-cons-threshold (* 2 1000 1000))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(zel yascroll yaml-mode which-key web-mode volatile-highlights validate use-package undo-tree typo treepy terraform-mode telephone-line symbol-overlay swoop sublimity smooth-scrolling smooth-scroll smex smart-mode-line slack rings rainbow-delimiters python-mode project-explorer pinentry paren-face paradox ov org-cliplink noflet neotree nav marshal markdown-mode+ magit lua-mode logview kv kpm-list kill-ring-search keyfreq key-chord ivy-prescient ivy-hydra inf-clojure iflipb ido-vertical-mode ido-ubiquitous ibuffer-vc hl-todo highlight-symbol helpful helm-projectile helm-open-github helm-descbinds google-translate google-this gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe+ git-gutter gist ghub+ frame-local focus flymake-cursor flycheck-joker flycheck-clojure flx-ido find-file-in-project face-explorer easy-kill dumb-jump dockerfile-mode dired+ diff-hl dash-functional csv-mode counsel company clojure-snippets cljr-helm cider-eval-sexp-fu centered-cursor-mode cbm bug-hunter buffer-stack browse-kill-ring benchmark-init beacon back-button avy ascii anaphora align-cljlet ag adaptive-wrap)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(put 'magit-edit-line-commit 'disabled nil)
-(put 'dired-find-alternate-file 'disabled nil)
-(put 'upcase-region 'disabled nil)
-(put 'narrow-to-region 'disabled nil)
-(put 'downcase-region 'disabled nil)
-
 (message "Done initializing.")
