22ea9ffe54e2cd1565fafe95bd8212804eaaddd7
Daemian Mack <daemianmack@gmail.com>
2020-07-01 14:05:34 -0400

    Relocate util to first of custom elisp stack

--- daemian.el
+++ daemian.el
@@ -6,8 +6,8 @@
 (setq custom-file (concat dotfiles-dir "custom.el"))
 (load custom-file 'noerror)
 
+(load (concat dotfiles-dir "util.el"))
 (load (concat dotfiles-dir "modes.el"))
 (load (concat dotfiles-dir "sets.el"))
-(load (concat dotfiles-dir "util.el"))
 (load (concat dotfiles-dir "experimental.el"))
 (load (concat dotfiles-dir "key_bindings.el"))
