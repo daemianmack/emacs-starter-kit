1524b349fc7d8d8025a03b8312cb9ae48b2866c5
Daemian Mack <daemianmack@gmail.com>
2020-07-31 12:06:01 -0400

    Udpate Melpa URLs

--- init.el
+++ init.el
@@ -18,9 +18,9 @@
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives
-             '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
 (package-initialize)
 
